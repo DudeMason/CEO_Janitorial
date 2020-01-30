ca0c57b814a65d31cab449e05199e32d9cd2bb5a
Mason Eyre <mason.deyre@gmail.com>
2020-01-29 17:18:10 -0700

    finished backend
    still need to make mailer

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,2 +1,38 @@
 class Api::AppointmentsController < ApplicationController
+
+  def index
+    render json: Appointment.all
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      render json: @appointment
+    else
+      render json: {errors: @appointment.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      render json: @appointment
+    else
+      render json: {errors: @appointment.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    render json: {message: 'Appointment deleted!'}
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(
+      :first_name, :last_name, :email, :phone, :company, :date, :time, :message
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  namespace :api do
+    resources :users
+    resources :appointments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
