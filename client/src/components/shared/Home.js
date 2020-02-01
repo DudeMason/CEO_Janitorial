acfaea2aab7184cd880c9bca67c2578785f363df
Mason Eyre <mason.deyre@gmail.com>
2020-02-01 16:49:23 -0700

    background center now

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -3,7 +3,7 @@ import Logo from './media/images/Logo2020.png';
 
 const Home = () => (
 
-  <div align='center' style={{paddingTop: 10, height: '100vh'}}>
+  <div align='center' style={{height: '100vh'}}>
     <img src={Logo} style={{opacity: .7}} width='75%' alt='CEO Janitorial Logo'></img>
   </div>
 )
