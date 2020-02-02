fb3496f03259d1aa7d2e03ae6ca626b2406b2885
Mason Eyre <mason.deyre@gmail.com>
2020-02-02 16:43:08 -0700

    Update NoMatch.js

--- client/src/components/shared/NoMatch.js
+++ client/src/components/shared/NoMatch.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Segment} from 'semantic-ui-react';
 
 const NoMatch = () => (
 
-  <div>
-    404 Error
-    <a href='/'>Home</a>
+  <div align='center' style={{height: '100vh'}}>
+    <Segment compact align='center' style={{padding: 100}}>
+      <p>
+        404 Error, this URL doesn't exist!
+        <br/>
+        Try another.
+      </p>
+      <a href='/'><u>Return Home</u></a>
+    </Segment>
   </div>
 )
 
