acd449537ae3e053d042107234f960e5ba2deb2b
Mason Eyre <mason.deyre@gmail.com>
2020-02-02 22:38:05 -0700

    Update Home.js
    .75 opacity

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -4,7 +4,7 @@ import Logo from './media/images/Logo2020.png';
 const Home = () => (
 
   <div align='center' style={{height: '100vh'}}>
-    <img src={Logo} style={{opacity: .7}} width='73%' alt='CEO Janitorial Logo'></img>
+    <img src={Logo} style={{opacity: .75}} width='73%' alt='CEO Janitorial Logo'></img>
   </div>
 )
 
