fb033d5b75d0cf561b6d8962782df83ac0e0b2fa
Mason Eyre <mason.deyre@gmail.com>
2020-02-02 22:31:45 -0700

    .

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -5,17 +5,6 @@ const Home = () => (
 
   <div align='center' style={{height: '100vh'}}>
     <img src={Logo} style={{opacity: .7}} width='73%' alt='CEO Janitorial Logo'></img>
-    <p style={{paddingBottom: 30, opacity: .8, paddingTop: 50}} align='center'>
-      © 2020 Website Created by Mason D Eyre
-      <br/>
-      <a href='mailto:mason.deyre@gmail.com' target='_blank' rel='noopener noreferrer'>
-        mason.deyre@gmail.com
-      </a>
-      <br/>
-      <a href='http://www.masoneyre.com' target='_blank' rel='noopener noreferrer'>
-        www.masoneyre.com
-      </a>
-    </p>
   </div>
 )
 
