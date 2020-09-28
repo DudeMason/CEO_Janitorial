ddbe042676239dc86aec9db4a60f7b9f485996da
Mason Eyre <mason.deyre@gmail.com>
2020-09-28 17:49:43 -0600

    Security update
    page goers were able to see appointments using dev-tools for React. Moved appointments to the Admin view.

--- client/src/components/auth/Admin.js
+++ client/src/components/auth/Admin.js
@@ -1,24 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 import { Segment, Grid } from 'semantic-ui-react';
 import { AppointmentConsumer } from "../../providers/AppointmentProvider";
 import Appointment from '../shared/Appointment';
 
-const Admin = ({ functions: {appointments}, functions }) => (
+class Admin extends React.Component{
 
-  <div align='center' style={{height: '100vh'}}>
-    <Segment align='center' compact>
-      <h1>Appointments</h1>
-    </Segment>
-    <br/>
-    <Grid columns='3' stackable className='grid'>
-    {
-      appointments.map( a =>
-        <Appointment key={a.id} {...a} functions={functions} />
-      )
-    }
-    </Grid>
-  </div>
-)
+  state = { appointments: [] }
+
+  componentDidMount() {
+    axios.get('/api/appointments')
+      .then( res => {
+        this.setState({ appointments: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  componentDidUpdate() {
+    axios.get('/api/appointments')
+      .then( res => {
+        this.setState({ appointments: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })  
+  }
+
+  render(){
+    const { functions } = this.props
+    const { appointments } = this.state
+
+    return(
+
+      <div align='center' style={{height: '100vh'}}>
+        <Segment align='center' compact>
+          <h1>Appointments</h1>
+        </Segment>
+        <br/>
+        <Grid columns='3' stackable className='grid'>
+        {
+          appointments.map( a =>
+            <Appointment key={a.id} {...a} functions={functions} />
+          )
+        }
+        </Grid>
+      </div>
+    )
+  }
+}
+
 export default class ConnectedAdmin extends React.Component {
   render() {
     return (
--- client/src/providers/AppointmentProvider.js
+++ client/src/providers/AppointmentProvider.js
@@ -6,24 +6,8 @@ export const AppointmentConsumer = AppointmentContext.Consumer;
 
 export default class AppointmentProvider extends Component {
 
-  state = { appointments: [] }
-
-  componentDidMount() {
-    axios.get('/api/appointments')
-      .then( res => {
-        this.setState({ appointments: res.data })
-      })
-      .catch( err => {
-        console.log(err)
-      })
-  }
-
   addAppoint = (appointment) => {
     axios.post('/api/appointments', appointment)
-      .then( res => {
-        const { appointments } = this.state
-        this.setState({ appointments: [...appointments, res.data]})
-      })
       .catch( err => {
         console.log(err)
       })
@@ -31,10 +15,6 @@ export default class AppointmentProvider extends Component {
 
   removeAppoint = (id) => {
     axios.delete(`/api/appointments/${id}`)
-      .then(res => {
-        const {appointments} = this.state
-        this.setState({ appointments: appointments.filter( a => a.id !==id ) })
-      })
       .catch (err => {
         console.log(err)
       })
@@ -42,14 +22,6 @@ export default class AppointmentProvider extends Component {
 
   editAppoint = (id, appointment) => {
     axios.put(`/api/appointments/${id}`, appointment)
-      .then( res => {
-        const appointments = this.state.appointments.map( a => {
-          if (a.id === id)
-          {return res.data}
-          return a
-        })
-        this.setState({ appointments })
-      })
       .catch(err => {
         console.log(err)
       })
@@ -58,7 +30,6 @@ export default class AppointmentProvider extends Component {
   render(){
     return(
       <AppointmentContext.Provider value={{
-        ...this.state,
         addAppoint: this.addAppoint,
         removeAppoint: this.removeAppoint,
         editAppoint: this.editAppoint,
