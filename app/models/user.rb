1164ff4606d5e0e84e3b9b3319bc324724d06a2f
Mason Eyre <mason.deyre@gmail.com>
2020-10-22 19:46:21 -0600

    adds tracking to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
-
 class User < ActiveRecord::Base
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
